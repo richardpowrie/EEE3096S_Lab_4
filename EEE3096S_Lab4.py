417f12cfd77223f90356aa3194e07784239bc387
richardpowrie <31163784+richardpowrie@users.noreply.github.com>
2018-09-05 19:05:38 +0200

    included code for interface with the ADC and functions for converting to relevant units

--- EEE3096S_Lab4.py
+++ EEE3096S_Lab4.py
@@ -10,6 +10,7 @@ import RPi.GPIO as GPIO
 import time
 import spidev
 import os
+import sys
 
 ##############################################################################
 #Define
@@ -25,7 +26,7 @@ button4  = 18 #display
 SPICLK   = 11          #connected to ADC pin 13
 SPIMOSI  = 10          #connected to ADC pin 11
 SPIMISO  = 9           #connected to ADC pin 12
-SPICS    = 8          #select ADC by pulling low, default high for no comms
+SPICS    = 8           #select ADC by pulling low, default high for no comms
 """ADC set up as follows:
 Channel Peripheral ADC pin Selection bits
 CH0     Pot        1       1000
@@ -47,55 +48,101 @@ VREF    3v3        15
                   = digital_output_code*3.3/1024
 VDD     3v3        16
 """
+#channels
+pot = 0
+therm = 1
+LDR = 2
 
 #frequencies
-f1       = 2   #every 500ms
-f2       = 1   #every second
-f3       = 0.5 #every 2s
+t1       = 0.5   #every 500ms
+t2       = 1   #every second
+t3       = 2 #every 2s
 
 #output
 outHeading = "Time      Timer     Pot    Temp  Light\n"#format: 2 spaces between columns
 outLines   = "--------------------------------------"
 outString  = outHeading+outLines
-dummyData  = 1111111111  #10 bits of dummy data
-##############################################################################
+
+decimal_places = 3
+
 
 ##############################################################################
 #SPI setup
 ##############################################################################
 spi = spidev.SpiDev()
-spi.open(0,0)
-##############################################################################
+spi.open(0,0)#bus #0 and device #0
+spi.max_speed_hz=1000000
+
 
 ##############################################################################
 #GPIO setup
 ##############################################################################
 #use GPIO BCM pin numbering
-GPIO.setmode(GPIO.BCM)              
+#GPIO.setmode(GPIO.BCM)              
 #outputs
-GPIO.setup(SPIMOSI,GPIO.OUT)
-GPIO.setup(SPIMISO, GPIO.IN)
-GPIO.setup(SPICLK, GPIO.OUT)
-GPIO.setup(SPICS, GPIO.OUT)
+#GPIO.setup(SPIMOSI,GPIO.OUT)
+#GPIO.setup(SPIMISO, GPIO.IN)
+#GPIO.setup(SPICLK, GPIO.OUT)
+#GPIO.setup(SPICS, GPIO.OUT)
 
 #set up buttons as digital inputs, using pull-up resistors
-GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(button3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(button4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(button3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(button4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 ##############################################################################
+#functions
+##############################################################################
+def GetData(channel):#channel = integer 0-7
+    adc = spi.xfer2([1,(8+channel)<<4,0])#send three bits with single ended mode selected
+    data = ((adc[1]&3)<<8) + adc[2]
+    return data
+
+# function to convert data to voltage level, 
+# places: number of decimal places needed 
+def ConvertVolts(data,places): 
+    volts = (data * 3.3) / float(1023)
+    volts = round(volts,places)
+    return volts
+    
+
+def convertTemp(data, places):
+    temp = (data-0.5)/0.010
+    #temp = ((data*330)/float(1023))-50
+    temp = round(temp,places)
+    return temp
 
 
 ##############################################################################
 #main
 ##############################################################################
-
-
-
-
-
-
+print(outString)
+delay = t1
+while True:
+    try:
+        #read pot
+        pot_data = GetData(pot)
+        Vpot = ConvertVolts(pot_data,decimal_places)
+        #read temp
+        therm_data = GetData(therm)
+        Vtherm = ConvertVolts(therm_data,decimal_places)
+        AmbTemp = convertTemp(Vtherm,decimal_places)
+        #read light
+        LDR_data = GetData(LDR)
+        VLDR = ConvertVolts(LDR_data,decimal_places)
+        #create output string
+        #output_string = "\t\t\t"+Vpot.toString()
+        output_string = "%10.3f abc" % Vpot
+        print(output_string)
+        print(Vpot)
+        print(AmbTemp)
+        print(VLDR)
+        break
+        time.sleep(delay)
+    except KeyboardInterrupt:
+        spi.close()
+        break
+        
 #release GPIO pins from operation
-GPIO.cleanup()
-##############################################################################
+#GPIO.cleanup()
